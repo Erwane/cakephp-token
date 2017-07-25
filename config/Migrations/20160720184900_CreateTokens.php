483fa6f1b925410170b5f4ea7466db5333e6aa4f
Erwane Breton <erwane@phea.fr>
2017-07-25 17:13:38 +0200

    rename token table and clean code

--- config/Migrations/20160720184900_CreateTokens.php
+++ config/Migrations/20160720184900_CreateTokens.php
@@ -1,47 +1,25 @@
 <?php
 
 use Migrations\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
 
 class CreateTokens extends AbstractMigration
 {
-
     public $autoId = false;
 
     public function change()
     {
-        $table = $this->table('tokens');
-        $table->addColumn('id', 'string', [
-            'limit' => 12,
-            'null' => false,
-            ])
-        ->addColumn('scope', 'string', [
-            'limit' => 50,
-            'default' => null,
-            'null' => true,
-            ])
-        ->addColumn('scope_id', 'integer', [
-            'signed' => false,
-            'limit' => 11,
-            'default' => null,
-            'null' => true,
-            ])
-        ->addColumn('type', 'string', [
-            'limit' => 64,
-            'null' => true,
-            ])
-        ->addColumn('content', 'text', [
-            'null' => true,
-            ])
-        ->addColumn('expire', 'datetime', [
-            'null' => false,
-            ])
-        ->addColumn('created', 'datetime', [
-            'null' => false,
-            ])
-        ->addPrimaryKey([
-            'id',
-            ])
-        ->addIndex(['scope', 'scope_id'])
-        ->create();
+        $table = $this->table('token_tokens');
+
+        $table->addColumn('id', 'string', [ 'limit' => 12, 'null' => false, ])
+            ->addColumn('scope', 'string', [ 'limit' => 50, 'default' => null, 'null' => true, ])
+            ->addColumn('scope_id', 'integer', [ 'signed' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'default' => null, 'null' => true, ])
+            ->addColumn('type', 'string', [ 'limit' => 64, 'null' => true, ])
+            ->addColumn('content', 'text', [ 'null' => true, ])
+            ->addColumn('expire', 'datetime', [ 'null' => false, ])
+            ->addColumn('created', 'datetime', [ 'null' => false, ])
+            ->addPrimaryKey([ 'id', ])
+            ->addIndex(['scope', 'scope_id'])
+            ->create();
     }
 }
