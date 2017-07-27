4989a752367af55ed24e6d422fad0e65bc387ebe
Erwane Breton <erwane@phea.fr>
2017-07-27 14:36:25 +0200

    simplify the token schema

---
+++ config/Migrations/20170727143023_SimplifyTokens.php
@@ -0,0 +1,34 @@
+<?php
+
+use Migrations\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class SimplifyTokens extends AbstractMigration
+{
+    public $autoId = false;
+
+    public function up()
+    {
+        $table = $this->table('token_tokens');
+
+        $table->removeIndex(['scope', 'scope_id']);
+
+        $table
+            ->removeColumn('scope')
+            ->removeColumn('scope_id')
+            ->removeColumn('type')
+            ->save();
+    }
+
+    public function down()
+    {
+        $table = $this->table('token_tokens');
+
+        $table
+            ->addColumn('scope', 'string', [ 'limit' => 50, 'default' => null, 'null' => true, ])
+            ->addColumn('scope_id', 'integer', [ 'signed' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'default' => null, 'null' => true, ])
+            ->addColumn('type', 'string', [ 'limit' => 64, 'null' => true, ])
+            ->addIndex(['scope', 'scope_id'])
+            ->save();
+    }
+}
