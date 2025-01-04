646e24d51db21576b1048daf372f3b4f27248807
Erwane <erwane@phea.fr>
2025-01-04 17:14:19 +0100

    Copyrights and badges

--- src/Model/Entity/Token.php
+++ src/Model/Entity/Token.php
@@ -1,6 +1,18 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * CakePHP Token
+ * Copyright (c) Erwane BRETON
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Erwane BRETON
+ * @see         https://github.com/Erwane/cakephp-token
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
 namespace Token\Model\Entity;
 
 use Cake\ORM\Entity;
@@ -8,7 +20,6 @@ use Cake\ORM\Entity;
 /**
  * Class Token
  *
- * @package Token\Model\Entity
  * @property string id
  * @property array content
  * @property \Cake\I18n\FrozenTime expire
